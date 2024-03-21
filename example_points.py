9dcd59b8d0584636fc8c816d6b8db764d05b3922
Christian Hauschel <hacs@zhaw.ch>
2024-03-21 09:37:22 +0100

    updates

--- example_points.py
+++ example_points.py
@@ -4,51 +4,137 @@ Script to test the most distant pair of points algorithm.
 https://www.baeldung.com/cs/most-distant-pair-of-points
 """
 
-# %% 
-
+# %%
 from copy import copy
-import proplot as pplt 
+import proplot as pplt
 from airfoil import Airfoil
 import numpy as np
 from scipy.spatial import ConvexHull
-from splines import Line, Spline 
+from splines import farthest_apart_points, area_tri, distance_pts, Line
+import numpy as np
+
+# enclose circle 
+import smallestenclosingcircle
+
+# %%
+
+# af = Airfoil.naca("0012", n=51)
+# af.add_TE_thickness(0.01)
+# points = af.data
+# points = np.roll(af.data, np.random.randint(0, af.n), axis=0)
 
 
-af = Airfoil.naca("0012", n=31)
-af.add_TE_thickness(0.01)
-points = af.data
-points = np.roll(af.data, 5, axis=0)
+# %%
 
+# random points
+# points = np.random.rand(6, 2)
+
+points = np.array(
+    [
+        [0.05328671, 0.75379551],
+        [0.22475366, 0.10027644],
+        [0.27405434, 0.5932981],
+        [0.35790505, 0.88768214],
+        [0.25576468, 0.44421372],
+        [0.90777934, 0.36292922],
+    ]
+)
 
 # randomize the index of the points
 np.random.shuffle(points)
-
 
 
 hull = ConvexHull(points)
 hull_indices = hull.vertices
 hull_points = points[hull.vertices]
 
+# create circle 
+center_x, center_y, radius = smallestenclosingcircle.make_circle(points)
 
 
+# distance, pt1, pt2 = farthest_apart_points(points)
 
-from splines import farthest_apart_points, area_tri, distance_pts
+hull = ConvexHull(points)
+hull_indices = hull.vertices
+hull_points = points[hull_indices]
+n = len(hull_points)
 
-pt1, pt2 = farthest_apart_points(points)
+k = 1
+while k < n:
+    crv_area = area_tri(hull_points[0], hull_points[n - 1], hull_points[k])
+    nxt_area = area_tri(hull_points[0], hull_points[n - 1], hull_points[k + 1])
+
+    if crv_area > nxt_area:
+        break
+
+    k += 1
+
+p = 0
+q = copy(k)
+
+answer = distance_pts(hull_points[p], hull_points[q])
+
+id1 = 0
+id2 = 0
+
+flag = False
+
+while p <= k and q < n:
+    while q < n:
+        crv_area = area_tri(hull_points[p], hull_points[p + 1], hull_points[q])
+        nxt_area = area_tri(
+            hull_points[p], hull_points[p + 1], hull_points[(q + 1) % n]
+        )
+
+        if crv_area > nxt_area:
+            break
+        
+        q += 1
+
+        print(answer)
+
+        if distance_pts(hull_points[p], hull_points[q % n]) > answer:
+            answer = distance_pts(hull_points[p], hull_points[q % n])
+            id1 = copy(p)
+            id2 = copy(q % n)
+            print("----")
+            print(answer)
+            print(id1)
+            print(id2)
+            print("----")
+    p += 1
+    
+
+print("answer", answer)
+print("diameter", radius*2)
+
+pt1 = hull_points[id1]
+pt2 = hull_points[id2]
 
 
-line_chord = Line(pt1, pt2)
+# center_x, center_y, radius = smallestenclosingcircle.make_circle(points)
 
 
-fig, ax = pplt.subplots(dpi=300, aspect="equal", figsize=(7,4))
+# line_chord = Line(pt1, pt2)
+
+
+fig, ax = pplt.subplots(dpi=300, aspect="equal", figsize=(7, 4))
+# plot circle
+# ax.plot(center_x, center_y, "o", label="Center of circle")
+
+
+# circle1 = plt.Circle((center_x, center_y), radius, color='r', fill=None)
+# plt.gca().add_patch(circle1)
+
 ax.plot(hull_points[:, 0], hull_points[:, 1], "-", alpha=1, label="Convex Hull")
+ax.format(xlim=(-0.01, 1.01))
 ax.plot(points[:, 0], points[:, 1], ".", label="Curve")
-line_chord.plot(ax)
+ax.plot(pt1[0], pt1[1], "x", label="Pt1")
+ax.plot(pt2[0], pt2[1], "x", label="Pt2")
+# line_chord.plot(ax)
 
 
 # Split crv by the line
 
 
-
 # %%
-
--- splines/points.py
+++ splines/points.py
@@ -2,177 +2,54 @@ from copy import copy
 import numpy as np
 from scipy.spatial import ConvexHull
 
-def distance_pts(p1, p2):
-    """Compute the distance between two points."""
-    return np.linalg.norm(p2 - p1)
 
-def area_tri(a, b, c):
-    """Compute the area of a triangle given its vertices."""
-    # Compute the vectors corresponding to the sides of the triangle
-    ab = b - a
-    ac = c - a
+def max_distant_pair(points):
+    CH = ConvexHull(points)
+    N = CH.vertices.size
+    K = 1
 
-    # Compute the cross product of the vectors
-    cross_product = np.cross(ab, ac)
-
-    # Return half of the magnitude of the cross product
-    return 0.5 * np.linalg.norm(cross_product)
-
-def farthest_apart_points(points: np.array) -> tuple:
-    """
-    Compute the most distant pair of points in a set of points using the 
-    rotate calipers approach [1].
-
-    Parameters
-    ----------
-    points : np.array
-        Array of points
-
-    Returns
-    -------
-    tuple
-        Tuple of the most distant pair of points
-
-    References
-    ----------
-    [1] https://www.baeldung.com/cs/most-distant-pair-of-points
-    """
-
-    hull = ConvexHull(points)
-    hull_indices = hull.vertices
-    hull_points = points[hull_indices]
-    n = len(hull_points)
-        
-    for k in range(n):
-        crv_area = area_tri(hull_points[0], hull_points[n-1], hull_points[k])
-        nxt_area = area_tri(hull_points[0], hull_points[n-1], hull_points[k+1])
-
-        if crv_area > nxt_area:
+    while K < N:
+        cur_area = area(
+            points[CH.vertices[0]], points[CH.vertices[N - 1]], points[CH.vertices[K]]
+        )
+        nxt_area = area(
+            points[CH.vertices[0]],
+            points[CH.vertices[N - 1]],
+            points[CH.vertices[K + 1]],
+        )
+        if cur_area > nxt_area:
             break
+        K += 1
 
-    p = 0
-    q = copy(k)
-
-    answer = distance_pts(hull_points[p], hull_points[q])
-
-    id1 = 0
-    id2 = 0
-
-    while p <= k and q < n:
-        while q < n:
-            crv_area = area_tri(hull_points[p], hull_points[p+1], hull_points[q])
-            nxt_area = area_tri(hull_points[p], hull_points[p+1], hull_points[(q+1) % n])
-
-            if crv_area > nxt_area:
+    P = 0
+    Q = K
+    answer = distance(points[CH.vertices[P]], points[CH.vertices[Q]])
+    pts_idx = (CH.vertices[P], CH.vertices[Q])
+    while P <= K and Q < N:
+        while Q < N:
+            cur_area = area(
+                points[CH.vertices[P]],
+                points[CH.vertices[P + 1]],
+                points[CH.vertices[Q]],
+            )
+            nxt_area = area(
+                points[CH.vertices[P]],
+                points[CH.vertices[P + 1]],
+                points[CH.vertices[(Q + 1) % N]],
+            )
+            if cur_area > nxt_area:
                 break
-
-            q += 1
-
-            if distance_pts(hull_points[p], hull_points[q % n]) > answer:
-                answer = distance_pts(hull_points[p], hull_points[q % n])
-                id1 = p
-                id2 = q % n
-
-        p += 1
-
-    return hull_points[id1], hull_points[id2]
+            Q += 1
+            if distance(points[CH.vertices[P]], points[CH.vertices[Q % N]]) > answer:
+                answer = distance(points[CH.vertices[P]], points[CH.vertices[Q % N]])
+                pts_idx = (CH.vertices[P], CH.vertices[Q % N])
+        P += 1
+    return answer, pts_idx
 
 
+def area(p1, p2, p3):
+    return 0.5 * np.abs(np.cross(p2 - p1, p3 - p1))
 
 
- 
-# import math
- 
-# # Define a class to represent a point
-# class Point:
-#     def __init__(self, x, y):
-#         self.x = x
-#         self.y = y
- 
-# # Function to calculate the 
-# # squared Euclidean distance between two points
-# def dist(p, q):
-#     return (p.x - q.x) ** 2 + (p.y - q.y) ** 2
- 
-# # Function to calculate the 
-# # absolute area of a triangle formed by three points
-# def absArea(p, q, r):
-#     return abs((p.x * q.y + q.x * r.y + r.x * p.y) -
-#                (p.y * q.x + q.y * r.x + r.y * p.x))
- 
-# # Function to calculate the 
-# # cross product of two vectors formed by three points
-# def crossProduct(p, q, r):
-#     return ((q.x - p.x) * (r.y - p.y)) - ((q.y - p.y) * (r.x - p.x))
- 
-# # Function to calculate the convex hull 
-# # of a list of points using the Graham scan algorithm
-# def convexHull(points):
-   
-#     # Sort the points lexicographically by their x-coordinates, 
-#     # breaking ties by their y-coordinates
-#     points.sort(key=lambda p: (p.x, p.y))
- 
-#     hull = []
-#     n = len(points)
-     
-#     # Traverse the sorted points from left to right
-#     for i in range(n):
-       
-#         # Remove any point from the hull that makes a 
-#         # clockwise turn with the previous two points on the hull
-#         while len(hull) >= 2 and crossProduct(hull[-2], hull[-1], points[i]) <= 0:
-#             hull.pop()
-             
-#         hull.append(points[i])
- 
-#     # Traverse the sorted points from right to left
-#     for i in range(n - 2, -1, -1):
-       
-#         # Remove any point from the hull that makes a 
-#         # clockwise turn with the previous two points on the hull
-#         while len(hull) >= 2 and crossProduct(hull[-2], hull[-1], points[i]) <= 0:
-#             hull.pop()
-             
-#         hull.append(points[i])
- 
-#     # Return the hull, omitting the last point, which is the same as the first point
-#     return hull[:-1]
- 
-# def rotatingCaliper(points):
-   
-#     # Takes O(n)
-#     convex_hull_points = convexHull(points)
-#     n = len(convex_hull_points)
- 
-#     # Convex hull point in counter-clockwise order
-#     hull = []
-#     for i in range(n):
-#         hull.append(convex_hull_points[i])
- 
-#     # Base Cases
-#     if n == 1:
-#         return 0
-#     if n == 2:
-#         return math.sqrt(dist(hull[0], hull[1]))
-#     k = 1
- 
-#     # Find the farthest vertex
-#     # from hull[0] and hull[n-1]
-#     while crossProduct(hull[n - 1], hull[0], hull[(k + 1) % n]) > crossProduct(hull[n - 1], hull[0], hull[k]):
-#         k += 1
- 
-#     res = 0
- 
-#     # Check points from 0 to k
-#     for i in range(k + 1):
-#         j = (i + 1) % n
-#         while crossProduct(hull[i], hull[(i + 1) % n], hull[(j + 1) % n]) > crossProduct(hull[i], hull[(i + 1) % n], hull[j]):
-#             # Update res
-#             res = max(res, math.sqrt(dist(hull[i], hull[(j + 1) % n])))
-#             id1 = i
-#             id2 = (j + 1) % n
-#             j = (j + 1) % n
- 
-#     # Return the result distance
-#     return res, id1, id2
+def distance(p1, p2):
+    return np.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
