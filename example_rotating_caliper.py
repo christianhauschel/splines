64449eb5a77a247da10000aab206b56b1a36c502
Christian Hauschel <hacs@zhaw.ch>
2024-03-21 10:31:43 +0100

    updates

---
+++ example_rotating_caliper.py
@@ -0,0 +1,35 @@
+# %%
+
+import numpy as np
+from scipy.spatial import ConvexHull
+from splines import max_distant_pair
+
+
+
+for i in range(20):
+
+    points = np.random.rand(10, 2)
+
+    # from airfoil import Airfoil 
+    # # generate random odd int 
+    # int_odd = np.random.randint(10, 101) * 2 + 1
+    # # random naca number 
+    # naca_id = np.random.randint(1000, 10000)
+    # af = Airfoil.naca(f"{naca_id:04d}", n=int_odd)
+    # points = af.data
+
+    max_dist, pts_idx = max_distant_pair(points)
+    print("Maximum distance:", max_dist, "Points:" , points[pts_idx[0]], points[pts_idx[1]])
+
+    p1 = points[pts_idx[0]]
+    p2 = points[pts_idx[1]]
+
+    import proplot as pplt 
+
+    fig, ax = pplt.subplots(aspect=1, figsize=(4,4))
+    ax.plot(points[:,0], points[:,1], 'o', color='black', markersize=2)
+    ax.plot([p1[0], p2[0]], [p1[1], p2[1]], 'r-')
+    fig.show
+
+
+# %%
